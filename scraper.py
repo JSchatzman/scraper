64b8a885a8f24f01d84d3ad45fd6fba7555641b6
jordan <j.schatzman@outlook.com>
2017-01-15 23:52:02 -0800

    adding inspection page

--- scraper.py
+++ scraper.py
@@ -1,5 +1,8 @@
 """Implementation of Scraper."""
 
+import requests
+from bs4 import BeautifulSoup
+
 INSPECTION_DOMAIN = 'http://info.kingcounty.gov'
 INSPECTION_PATH = '/health/ehs/foodsafety/inspections/Results.aspx'
 INSPECTION_PARAMS = {
@@ -20,3 +23,21 @@ INSPECTION_PARAMS = {
     'Fuzzy_Search': 'N',
     'Sort': 'H'
 }
+
+
+def get_inspection_page(**kwargs):
+    """Retrieve the inspection page."""
+    url = INSPECTION_DOMAIN + INSPECTION_PATH
+    params = INSPECTION_PARAMS.copy()
+    for key, val in kwargs.items():
+        if key in INSPECTION_PARAMS:
+            params[key] = val
+    resp = requests.get(url, params=params)
+    resp.raise_for_status()
+    return resp.content, resp.encoding
+
+
+def write_file(scraped_html):
+    """Write results of scraped html to a file."""
+    with open('inspection_page.html', 'w') as t:
+        t.write(scraped_html)
